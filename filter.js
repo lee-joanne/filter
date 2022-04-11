203d308585c86607f280d12f60d6fe1d73526ff1
Joanne <lee.joanne54@gmail.com>
2022-04-11 21:11:24 +0000

    Initial commit

--- filter.js
+++ filter.js
@@ -1,4 +1,4 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node filter.js in the terminal
@@ -20,7 +20,11 @@ const people = [
     age: 18,
   },
 ];
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
 
+const paul = people.filter(person => person.name === "Paul");
+console.log(paul)
 
 // Complex Filtering
 const students = [
@@ -55,3 +59,14 @@ const students = [
     ]
   },
 ];
+
+// const candidates = students.filter(student => {
+//   let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+//   return strongSkills.length > 0;
+// })
+// console.log(candidates)
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
